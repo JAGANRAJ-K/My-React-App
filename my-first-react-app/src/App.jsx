12d908749fb4c535083ced206b72e73343101068
JAGANRAJ-K <jaganrajk000@gmail.com>
2026-04-22 15:27:26 +0530

    initial commit

---
+++ my-first-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Demo from "./Demo";
+import User from "./User";
+
+let App =()=>{
+    return <div>
+        {/* <h1>App Component</h1> */}
+       {/* <Demo/> */}
+       <User/>
+    </div>;
+}
+
+export default App;
---
+++ my-first-react-app/src/Demo.jsx
@@ -0,0 +1,5 @@
+let Demo=()=>{
+    return <h1>Demo Component</h1>
+}
+
+export default Demo;
